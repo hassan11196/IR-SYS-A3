eed092726532fc6919e5017689a72d55ba33aec6
hassan11196 <hassan11196@hotmail.com>
2020-04-10 20:51:10 +0500

    Vector Space Basic App

--- app/AxiosProxy.js
+++ app/AxiosProxy.js
@@ -4,5 +4,5 @@ export default axios.create({
   baseURL:
     process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true'
       ? 'http://localhost:8000'
-      : 'http://ira1.herokuapp.com'
+      : 'http://ira2.herokuapp.com'
 });
